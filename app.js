0b6d1f865938b75f0bd9c27bda24608e51204372
SadiqMomin <99909596+SadiqMomin@users.noreply.github.com>
2022-05-08 13:04:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+  name: 'Lernantino',
+  age: 99
+};
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
