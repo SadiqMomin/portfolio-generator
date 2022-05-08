f90378ecd9a256f9631b569d151b6645e7767346
SadiqMomin <99909596+SadiqMomin@users.noreply.github.com>
2022-05-08 13:15:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
 const printProfileData = (profileDataArr) => {
   console.log(profileDataArr);
 };
